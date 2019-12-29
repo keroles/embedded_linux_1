997210f4cf81912ffcaf1eceedadb6c69505e12b
keroles <eng.keroles.karam@gmail.com>
2019-12-29 14:36:47 +0200

    manage folders

---
+++ Projects/Project_1_part4/implementation_solutiones/ahmed_nader/Client_ECU/threads.h
@@ -0,0 +1,65 @@
+/*
+ * threads.h
+ *
+ *  Created on: Dec 24, 2019
+ *      Author: embedded_system_ks
+ */
+
+#ifndef THREADS_H_
+#define THREADS_H_
+
+/*********************** includes **********************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+#include "ethernet_socket.h"
+
+
+/*********************** Definitions ***********************/
+#define TIME_SIZE 		24
+#define DATA_MAX_SIZE 	64
+
+#define FILE_PATH "./dataLog.txt"
+/*********************** Decelerations ***********************/
+
+#pragma pack(push,1)
+struct packet_struct {
+	unsigned short ID;
+	unsigned char DLC;
+	char data[DATA_MAX_SIZE + 2*TIME_SIZE];
+
+}packet;
+#pragma pack(pop)
+
+typedef struct packet_struct packet_t;
+
+typedef union {
+	packet_t packet;
+	char packetInBytes[sizeof(packet)];
+} canPacket;
+
+extern pthread_mutex_t recvMutex;
+extern int file_fd;
+extern canPacket receivedPacket;
+
+/*********************** Prototypes **********************************/
+void * Thread_WriteFile(void * arg);
+void * Thread_SocketClient(void * arg);
+void write_in_file(void);
+
+#endif /* THREADS_H_ */
---
+++ Projects/Project_1_part4/implementation_solutiones/ahmed_nader/Server_ECU/ethernet_socket.c
@@ -0,0 +1,44 @@
+/*
+ * ethernet_socket.c
+ *
+ *  Created on: Dec 23, 2019
+ *      Author: Ahmed Nader
+ */
+#include "ethernet_socket.h"
+
+int new_socket;
+int server_fd;
+
+void ethernetSocketServer_init(void) {
+
+	struct sockaddr_in address;
+
+	memset(&address, 0x00, sizeof(address));
+
+	// Creating socket file descriptor
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
+
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = htonl(INADDR_ANY);
+	address.sin_port = htons(PORT);
+
+	// Forcefully attaching socket to the port
+	if (bind(server_fd, (struct sockaddr *) &address, sizeof(address)) < 0) {
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+
+	if (listen(server_fd, 10) < 0) {
+		perror("listen");
+		exit(EXIT_FAILURE);
+	}
+
+	if ((new_socket = accept(server_fd, NULL, NULL)) < 0) {
+		perror("accept");
+		exit(EXIT_FAILURE);
+	}
+	printf("connection established \n");
+}
