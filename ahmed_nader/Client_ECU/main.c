955ccecfac7b7bb5bc0cbb6a2ff324add83421b9
ahmednader284 <59331471+ahmednader284@users.noreply.github.com>
2019-12-29 13:08:24 +0200

    Create main.c

---
+++ ahmed_nader/Client_ECU/main.c
@@ -0,0 +1,36 @@
+/*
+ * main.c
+ *
+ *  Created on: Dec 23, 2019
+ *      Author: Ahmed Nader
+ */
+ 
+#include "threads.h"
+#include "my_signals.h"
+
+canPacket receivedPacket;
+
+int main(int argc, char * argv[]) {
+
+	pthread_t clientSocket_thread;
+	pthread_t dumpFile_thread;
+
+	//acquire mutex
+	pthread_mutex_lock(&recvMutex);
+	//Create threads
+	signal(SIGINT, int_Signal_handler);
+
+	if (pthread_create(&clientSocket_thread, NULL, Thread_SocketClient, argv[1])
+			< 0) {
+		perror("clientSocket_thread");
+		exit(0);
+	}
+	if (pthread_create(&dumpFile_thread, NULL, Thread_WriteFile, NULL) < 0) {
+		perror("dumpFile_thread");
+		exit(0);
+	}
+
+//join threads
+	pthread_join(clientSocket_thread, NULL);
+	pthread_join(dumpFile_thread, NULL);
+}
